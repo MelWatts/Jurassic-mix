d896640d862770e2818abc5380761edad295622e
Mel Watts <mel.watts@therooflightcompany.co.uk>
2021-07-03 15:34:34 +0000

    Start adding functions to the memory cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,85 @@
  * GENERAL VARIABLES
  */
  
-document.addEventListener('DomContentLoaded', () => {
-    const timeLeftDisplay = document.querySelector('#time-left')
-    const startBtn = document.querySelector('#start-button')
-    let timeLeft = 10
+//card options
 
-    function countDown(){
-        setInterval(function(){
-            if(timeLeft <= 0 ) {
-                clearInterval(timeLeft = 0)
-            }
-            timeLeftDisplay.innerHTML = timeLeft
-            timeLeft -=1
-        }, 1000)
-    }
-
-    startBtn.addEventListener('click', countDown)
+const cardArray = [
+    {
+        name: 'dino-1',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-1',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-2',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-2',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-3',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-3',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-4',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-4',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-5',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-5',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-6',
+        img: 'image/dino-1.jpg'
+    },
+    {
+        name: 'dino-6',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-7',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-7',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-8',
+        img: 'images/dino-1.jpg'
+    },
+    {
+        name: 'dino-8',
+        img: 'images/dino-1.jpg'
+    },
+]
 })
+
+const grid = document.querySelector('.grid')
+
+//create your board
+
+function creatBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src' 'images/blank.jpg')
+        card.setAttribute('data-id', i)
+        card.addEventListener
+    }
+}
