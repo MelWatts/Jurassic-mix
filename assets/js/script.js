19cb654513a9e27aace89523624b1d1019f59baf
Mel Watts <mel.watts@therooflightcompany.co.uk>
2021-07-09 15:22:28 +0000

    Add script for matching cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,15 +48,14 @@ class MixOrMatch {
         this.timeRemaining = this.totalTime;
         this.matchedCards = [];
         this.busy = true;
-
-       setTimeout(() => {
+        setTimeout(() => {
            this.audioController.startMusic();
            this.shuffleCards();
            this.countDown = this.startCountDown();
            this.busy = false;
         }, 500);
         this.hideCards();
-        this.timer.innerText = this.timerRemaining;
+        this.timer.innerText = this.timeRemaining;
         this.ticker.innerText = this.totalClicks;
     }
     hideCards() {
@@ -73,15 +72,48 @@ class MixOrMatch {
             this.ticker.innerText = this.totalClicks;
             card.classList.add('visible');
 
-            //if statement
+            if(this.cardToCheck)
+                this.checkForCardMatch(card);
+            else
+                this.cardToCheck = card;
         }
+    }
+    checkForCardMatch(card) {
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck);
+        else
+            this.cardMisMatch(card, this.cardToCheck);
+
+            this.cardToCheck = null;
+    }
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        this.audioController.match();
+        if(this.matchedCards.length === this.cardsArray)
+            this.victory();
+    }
+
+    cardMisMatch(card1, card2) {
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible');
+            card2.classList.remove('visible');
+            this.busy = false;
+        }, 1000);
+    }
+
+    getCardType(card) {
+        return card.getElementsByClassName('card-value')[0].src;
     }
 
     startCountDown() {
         return setInterval(() => {
             this.timeRemaining --;
             this.timer.innerText = this.timeRemaining;
-            if(this.timeRemaining ===0)
+            if(this.timeRemaining === 0)
             this.gameOver();
         }, 1000);
     }
@@ -95,7 +127,7 @@ class MixOrMatch {
     victory() {
         clearInterval(this.countDown);
         this.audioController.victory();
-        document.getElementById('game-over').classList.add('visible');
+        document.getElementById('victory-text').classList.add('visible');
     }
 
     shuffleCards()  {
@@ -108,8 +140,7 @@ class MixOrMatch {
 
 
     canFlipCard(card) {
-        return true;
-        //return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck
     }
 }
 
