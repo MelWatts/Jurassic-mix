f578cf5201defe18304071d81947b634e413eff2
Mel Watts <mel.watts@therooflightcompany.co.uk>
2021-07-09 12:16:21 +0000

    Add all the audio for match, flip, game over and victory

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 class AudioController {
     constructor() {
         this.bgMusic = new Audio('assets/audio/roar.mp3');
-        this.flipSound = new Audio('assets/audio/roar.mp3');
-        this.matchSound = new Audio('assets/audio/roar.mp3');
-        this.victorySound = new Audio('assets/audio/roar.mp3');
-        this.gameOverSound = new Audio('assets/audio/roar.mp3');
+        this.flipSound = new Audio('assets/audio/flip.wav');
+        this.matchSound = new Audio('assets/audio/match.wav');
+        this.victorySound = new Audio('assets/audio/victory.wav');
+        this.gameOverSound = new Audio('assets/audio/game-over.wav');
         this.bgMusic.volume = 0.5;
         this.bgMusic.loop = true;
     }
     startMusic() {
         this.bgMusic.play();
+    }
+    stopMusic() {
+        this.bgMusic.pause();
+        this.bgMusic.currentTime = 0;
+    }
+    flip() {
+        this
+
     }
 }
 
